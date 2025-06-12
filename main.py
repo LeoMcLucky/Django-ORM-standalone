f51f49fe1e30509dbfbbf8a70f0e3c72e508694c
Lev <leon.fissenko@gmail.com>
2025-06-12 14:50:29 +0500

    Добавил README

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Общее количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Количество активных пропусков:', len(
+        Passcard.objects.filter(is_active=True)))
--- settings.py
+++ settings.py
@@ -1,17 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.environ['DB_SECRET_KEY']
 
 TIME_ZONE = 'Europe/Moscow'
 
